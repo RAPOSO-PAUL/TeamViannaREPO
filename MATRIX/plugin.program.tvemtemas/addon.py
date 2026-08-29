38d7e648633e53b1ca3ddd4aad1df63d75c3e885
RAPOSO-PAUL <60862476+RAPOSO-PAUL@users.noreply.github.com>
2026-08-29 15:41:40 -0300

    Add files via upload

---
+++ MATRIX/plugin.program.tvemtemas/addon.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os, sys, importlib
+try:
+    import xbmcaddon, xbmcvfs
+    _base = xbmcvfs.translatePath(xbmcaddon.Addon().getAddonInfo('path'))
+except Exception:
+    _base = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(_base, 'resources', 'lib'))
+_mod = 'tvemtemas_main_%d_%d' % (sys.version_info[0], sys.version_info[1])
+try:
+    _m = importlib.import_module(_mod)
+    if hasattr(_m, 'escolher'):
+        _m.escolher()
+except Exception:
+    try:
+        import xbmcgui
+        xbmcgui.Dialog().ok('TV EMINEM - Temas',
+            'Sem bytecode para Python %d.%d neste pacote. '
+            'Recompile incluindo essa versao.'
+            % (sys.version_info[0], sys.version_info[1]))
+    except Exception:
+        pass
+    raise
